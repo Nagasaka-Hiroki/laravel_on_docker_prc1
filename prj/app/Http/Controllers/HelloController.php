6d8d21c2485f40d83d76ebe97c58b0ce149f32b3
Nagasaka Hiroki <blue.sapphire.ore@gmail.com>
2023-08-03 18:49:56 +0900

    HelloControllerの追加

---
+++ prj/app/Http/Controllers/HelloController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    //
+    public function index($id='noname', $pass='unknown') {
+        return <<<EOF
+<html>
+<head>
+<title>hello/index</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1   {font-size:100pt; text-align:right; color:#eee;
+      margin:-40px 9px -50px 0px;}
+</style>
+</head>
+<body>
+    <h1>Index</h1>
+    <p>これは、Helloコントローラのindexアクションです。</p>
+    <ul>
+        <li>ID: {$id}</li>
+        <li>PASS: {$pass}</li>
+</body>
+</html>
+EOF;
+    }
+}

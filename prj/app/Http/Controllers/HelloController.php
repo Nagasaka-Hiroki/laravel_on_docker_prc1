04c6a8b87a6b1199955cea620bab2607bd3063b9
Nagasaka Hiroki <blue.sapphire.ore@gmail.com>
2023-08-04 20:52:51 +0900

    クエリパラメータの利用について

--- prj/app/Http/Controllers/HelloController.php
+++ prj/app/Http/Controllers/HelloController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class HelloController extends Controller
 {
     //
-    public function index() {
-        $data=['msg'=>'これはコントローラから渡されたメッセージです。'];
+    public function index(Request $request) {
+        $data=['msg'=>'これはコントローラから渡されたメッセージです。',
+               'id'=>$request->id,
+              ];
         return view('hello.index',$data);
     }
 }

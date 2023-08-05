bc5c673c0a7e1c9afd84ac20efe384eef268f98b
Nagasaka Hiroki <blue.sapphire.ore@gmail.com>
2023-08-05 22:33:53 +0900

    @issetディレクティブで書き直した場合

--- prj/app/Http/Controllers/HelloController.php
+++ prj/app/Http/Controllers/HelloController.php
@@ -8,12 +8,10 @@ class HelloController extends Controller
 {
     //
     public function index() {
-        $data=['msg'=>''];
-        return view('hello.index',$data);
+        return view('hello.index');
     }
 
     public function post(Request $request){
-        $msg=$request->msg;
         return view('hello.index',['msg'=>$request->msg]);
     }
 }
